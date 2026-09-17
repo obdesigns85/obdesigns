66429cbffaa49b91b3b2d8444c8c379c64ca5587
obdesigns85 <obdesigns85@gmail.com>
2026-09-17 22:19:50 +0100

    Add 'Wall Paneling' to project filters

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -7,8 +7,7 @@ import BeforeAfterSlider from "../components/BeforeAfterSlider";
 import { useSEO } from "../lib/useDocumentTitle";
 import "../styles/projects.css";
 
-const filters: ("All" | ProjectCategory)[] = ["All", "Painting", "Screeding", "POP Installations", "Interiors", "Exteriors"];
-
+const filters: ("All" | ProjectCategory)[] = ["All", "Painting", "Screeding", "POP Installations", "Interiors", "Exteriors", "Wall Paneling"];
 export default function Projects() {
   useSEO({
     title: "Our Projects | OB Designs & Interiors Portfolio",
