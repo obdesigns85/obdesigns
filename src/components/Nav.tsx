1127ec316a9a52550bab2c00447d12e1ff312388
obdesigns85 <obdesigns85@gmail.com>
2026-09-20 22:26:06 +0100

    Rename '360° Tour' link to 'Virtual Tour'

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -35,7 +35,7 @@ export default function Nav() {
           <Link to="/services" onClick={() => setOpen(false)}>Services</Link>
           <Link to="/real-estate" onClick={() => setOpen(false)}>Real Estate</Link>
           <Link to="/projects" onClick={() => setOpen(false)}>Projects</Link>
-          <Link to="/virtual-tour" onClick={() => setOpen(false)}>360° Tour</Link>
+          <Link to="/virtual-tour" onClick={() => setOpen(false)}>Virtual Tour</Link>
           <Link to="/contact" onClick={() => setOpen(false)}>Contact</Link>
           <Link to="/consultation" className="navCta" onClick={() => setOpen(false)}>Request a Free Quote</Link>
         </div>
